148dffb9e2ec8c0b101693169ff416dd255e6782
Eric John <ericjohn@EricdeMacBook-Air.local>
2023-02-28 23:02:42 +0800

    update some code

---
+++ packages/eventpool/pool.js
@@ -0,0 +1,42 @@
+/**
+ * @description
+ * This module is designed to manage some tasks by asynchronous
+ */
+
+class TaskManager {
+  taskQueue = null;
+  maxTaskCount = null;
+  currentTaskCount = null;
+
+  /**
+   * 
+   * @param { number } taskCount Maximum tasks can be running
+   */
+  constructor(taskCount) {
+    this.taskQueue = [];
+    this.maxTaskCount = taskCount;
+    this.currentTaskCount = 0;
+  }
+
+  /**
+   * 
+   * @param { <T, R>(...args: R[]) => T | Promise<T>} task 
+   */
+  addTask(task) {
+    if (typeof task !== 'function') {
+      return;
+    }
+
+    this.taskQueue.push(task);
+  }
+
+  runTask() {
+
+  }
+
+  endTask() {
+    
+  }
+}
+
+new TaskManager();
